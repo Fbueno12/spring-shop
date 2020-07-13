3c9f94a30dbd092b299639daf8e340d9603e5eb3
Felipe Bueno <felipebueno181@gmail.com>
2020-07-13 15:06:43 -0300

    configuring initial category domain

--- src/main/java/com/felipebueno/springshop/resources/CategoryResource.java
+++ src/main/java/com/felipebueno/springshop/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.felipebueno.springshop.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.felipebueno.springshop.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is Working";
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 }
