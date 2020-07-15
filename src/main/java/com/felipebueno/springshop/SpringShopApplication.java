6e0e970e73df1cfde31d6f53dd607de516084a2d
Felipe Bueno <felipebueno181@gmail.com>
2020-07-15 10:53:52 -0300

    criando entidades de cidade e estado

--- src/main/java/com/felipebueno/springshop/SpringShopApplication.java
+++ src/main/java/com/felipebueno/springshop/SpringShopApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.felipebueno.springshop.domain.Category;
+import com.felipebueno.springshop.domain.City;
 import com.felipebueno.springshop.domain.Product;
+import com.felipebueno.springshop.domain.State;
 import com.felipebueno.springshop.repositories.CategoryRepository;
+import com.felipebueno.springshop.repositories.CityRepository;
 import com.felipebueno.springshop.repositories.ProductRepository;
+import com.felipebueno.springshop.repositories.StateRepository;
 
 @SpringBootApplication
 public class SpringShopApplication implements CommandLineRunner{
@@ -20,6 +24,12 @@ public class SpringShopApplication implements CommandLineRunner{
 	
 	@Autowired
 	ProductRepository productRepository;
+	
+	@Autowired
+	StateRepository stateRepository;
+	
+	@Autowired
+	CityRepository cityRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShopApplication.class, args);
@@ -34,16 +44,28 @@ public class SpringShopApplication implements CommandLineRunner{
 		Product p2 = new Product(null, "Impressora", 800.00);
 		Product p3 = new Product(null, "Mouse", 80.00);
 		
-		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
 		
 		p1.getCategories().addAll(Arrays.asList(cat1));
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1, cat2));
 		
-		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+		
+		City c1 = new City(null, "Uberlândia", st1);
+		City c2 = new City(null, "São Paulo", st2);
+		City c3 = new City(null, "Campinas", st2);
+		
+		st1.getCities().addAll(Arrays.asList(c1));
+		st2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
