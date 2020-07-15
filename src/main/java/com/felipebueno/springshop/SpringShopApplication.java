2abdd95dd27aad532afefee49254f374b5553554
Felipe Bueno <felipebueno181@gmail.com>
2020-07-15 10:15:54 -0300

    criando relacionamento produto-categoria

--- src/main/java/com/felipebueno/springshop/SpringShopApplication.java
+++ src/main/java/com/felipebueno/springshop/SpringShopApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.felipebueno.springshop.domain.Category;
+import com.felipebueno.springshop.domain.Product;
 import com.felipebueno.springshop.repositories.CategoryRepository;
+import com.felipebueno.springshop.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringShopApplication implements CommandLineRunner{
 
 	@Autowired
 	CategoryRepository categoryRepository;
+	
+	@Autowired
+	ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShopApplication.class, args);
@@ -25,7 +30,20 @@ public class SpringShopApplication implements CommandLineRunner{
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
 		
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1, cat2));
+		
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
