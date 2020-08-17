50bd38cf4e0e2907afa8648f2e27b14fb0c111a5
Felipe Bueno <felipebueno181@gmail.com>
2020-08-17 18:37:17 -0300

    Client, type and address entities

--- src/main/java/com/felipebueno/springshop/SpringShopApplication.java
+++ src/main/java/com/felipebueno/springshop/SpringShopApplication.java
@@ -7,12 +7,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.felipebueno.springshop.domain.Address;
 import com.felipebueno.springshop.domain.Category;
 import com.felipebueno.springshop.domain.City;
+import com.felipebueno.springshop.domain.Client;
 import com.felipebueno.springshop.domain.Product;
 import com.felipebueno.springshop.domain.State;
+import com.felipebueno.springshop.domain.enums.ClientType;
+import com.felipebueno.springshop.repositories.AddressRepository;
 import com.felipebueno.springshop.repositories.CategoryRepository;
 import com.felipebueno.springshop.repositories.CityRepository;
+import com.felipebueno.springshop.repositories.ClientRepository;
 import com.felipebueno.springshop.repositories.ProductRepository;
 import com.felipebueno.springshop.repositories.StateRepository;
 
@@ -30,6 +35,12 @@ public class SpringShopApplication implements CommandLineRunner{
 	
 	@Autowired
 	CityRepository cityRepository;
+	
+	@Autowired
+	ClientRepository clientRepository;
+
+	@Autowired
+	AddressRepository addressRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringShopApplication.class, args);
@@ -66,6 +77,18 @@ public class SpringShopApplication implements CommandLineRunner{
 		
 		stateRepository.saveAll(Arrays.asList(st1, st2));
 		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Client cli1 = new Client(null, "Maria Silva", "Maria@gmail.com", "36378912377", ClientType.PESSOAFISICA);
+		cli1.getTelephones().addAll(Arrays.asList("27363323", "93838398"));
+		
+		Address e1 = new Address(null, "Rua Flores", "300", "Apto 303", "Jardins", "382208384", cli1, c1);
+		Address e2 = new Address(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getAddresses().addAll(Arrays.asList(e1, e2));
+		
+		clientRepository.saveAll(Arrays.asList(cli1));
+		addressRepository.saveAll(Arrays.asList(e1, e2));
+		
 	}
 
 }
