cade38d7411c51effdf3d0a6354bc80af58830c5
fbueno12 <felipebueno181@gmail.com>
2020-09-02 12:30:05 -0300

    endpoint clients and prevent serialization

--- src/main/java/com/felipebueno/springshop/resources/ClientResource.java
+++ src/main/java/com/felipebueno/springshop/resources/ClientResource.java
@@ -2,27 +2,26 @@ package com.felipebueno.springshop.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.felipebueno.springshop.domain.Category;
-import com.felipebueno.springshop.services.CategoryService;
+import com.felipebueno.springshop.domain.Client;
+import com.felipebueno.springshop.services.ClientService;
 
 @RestController
-@RequestMapping(value="/categories")
-public class CategoryResource {
+@RequestMapping(value="/clients")
+public class ClientResource {
 	
 	@Autowired
-	private CategoryService service;
+	private ClientService service;
 	
 	@RequestMapping(method=RequestMethod.GET, value="/{id}" )
-	public ResponseEntity<Category> find(@PathVariable Integer id) {
-		Category category = service.find(id);
+	public ResponseEntity<Client> find(@PathVariable Integer id) {
+		Client client = service.find(id);
 		
-		return ResponseEntity.ok().body(category);
+		return ResponseEntity.ok().body(client);
 	}
 	
 }
